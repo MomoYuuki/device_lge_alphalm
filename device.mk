312ab005395f8a4a7178041f7a29a98f061a7184
MomoYuuki <108107780+MomoYuuki@users.noreply.github.com>
2023-10-27 13:49:41 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,13 +17,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/lge/flashlmdd/flashlmdd-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/alphalm/alphalm-vendor.mk)
 
-DEVICE_PATH := device/lge/flashlmdd
-DEVICE_NAME := flashlmdd
+DEVICE_PATH := device/lge/alphalm
+DEVICE_NAME := alphalm
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # common flash
-$(call inherit-product, device/lge/flash-common/flash.mk)
+$(call inherit-product, device/lge/alpha-common/alpha.mk)
